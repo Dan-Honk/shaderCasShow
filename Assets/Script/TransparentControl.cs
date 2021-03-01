14a2f682d9c0e44cffc9861b6a9973e8e174b8a9
danee <763511736@qq.com>
2021-03-01 23:19:38 +0800

    delete post effect file,will be done after i learn post effect more

---
+++ Assets/Script/TransparentControl.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransparentControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public class TransparentParm
+    {
+        public Material[] materials = null;
+        public Material[] shaderMats = null;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
