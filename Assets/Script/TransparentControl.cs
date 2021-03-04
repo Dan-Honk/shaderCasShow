c6f06bc84bf45254eec77f14ce2212427baff3b0
danee <763511736@qq.com>
2021-03-04 23:31:24 +0800

    commit Lather effect

--- Assets/Script/TransparentControl.cs
+++ Assets/Script/TransparentControl.cs
@@ -1,32 +1,38 @@
-﻿using System.Collections;
+﻿/********************************************************************
+ FileName: TransparentControl.cs
+ Description: 遮挡半透查询控制器，挂于摄像机
+ Created: 2017/07/23
+ history: 23:7:2017 12:45 by puppet_master
+*********************************************************************/
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class TransparentControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    public class TransparentParm
+
+    public class TransparentParam
     {
         public Material[] materials = null;
-        public Material[] shaderMats = null;
+        public Material[] sharedMats = null;
         public float currentFadeTime = 0;
         public bool isTransparent = true;
     }
 
-    public Transform targetObject = null;
-    public float height = 3.0f;
-    public float destTransparent = 0.2f;
-    public float fadeInTime = 1.0f;
-    private int transparentLayer;
-    private Dictionary<Renderer, TransparentParm> transparentDic = new Dictionary<Renderer, TransparentParm>();
+    public Transform targetObject = null;   //目标对象
+    public float height = 3.0f;             //目标对象Y方向偏移
+    public float destTransparent = 0.2f;    //遮挡半透的最终半透强度，
+    public float fadeInTime = 1.0f;         //开始遮挡半透时渐变时间
+    private int transparentLayer;           //需要遮挡半透的层级
+    private Dictionary<Renderer, TransparentParam> transparentDic = new Dictionary<Renderer, TransparentParam>();
     private List<Renderer> clearList = new List<Renderer>();
 
-    private void Start()
+    void Start()
     {
         transparentLayer = 1 << LayerMask.NameToLayer("OcclusionTran");
     }
 
-    // Update is called once per frame
     void Update()
     {
         if (targetObject == null)
@@ -39,9 +45,9 @@ public class TransparentControl : MonoBehaviour
     public void UpdateTransparentObject()
     {
         var var = transparentDic.GetEnumerator();
-        while(var.MoveNext())
+        while (var.MoveNext())
         {
-            TransparentParm param = var.Current.Value;
+            TransparentParam param = var.Current.Value;
             param.isTransparent = false;
             foreach (var mat in param.materials)
             {
@@ -57,13 +63,15 @@ public class TransparentControl : MonoBehaviour
     public void UpdateRayCastHit()
     {
         RaycastHit[] rayHits = null;
+        //视线方向为从自身（相机）指向目标位置
         Vector3 targetPos = targetObject.position + new Vector3(0, height, 0);
         Vector3 viewDir = (targetPos - transform.position).normalized;
         Vector3 oriPos = transform.position;
         float distance = Vector3.Distance(oriPos, targetPos);
         Ray ray = new Ray(oriPos, viewDir);
         rayHits = Physics.RaycastAll(ray, distance, transparentLayer);
-        Debug.DrawLine(oriPos, viewDir);
+        //直接在Scene画一条线，方便观察射线
+        Debug.DrawLine(oriPos, targetPos, Color.red);
         foreach (var hit in rayHits)
         {
             Renderer[] renderers = hit.collider.GetComponentsInChildren<Renderer>();
@@ -78,11 +86,12 @@ public class TransparentControl : MonoBehaviour
     {
         clearList.Clear();
         var var = transparentDic.GetEnumerator();
-        while(var.MoveNext())
+        while (var.MoveNext())
         {
-            if(var.Current.Value.isTransparent == false)
+            if (var.Current.Value.isTransparent == false)
             {
-                var.Current.Key.materials = var.Current.Value.shaderMats;
+                //用完后材质实例不会销毁，可以被unloadunuseasset销毁或切场景销毁。
+                var.Current.Key.materials = var.Current.Value.sharedMats;
                 clearList.Add(var.Current.Key);
             }
         }
@@ -92,13 +101,14 @@ public class TransparentControl : MonoBehaviour
 
     void AddTransparent(Renderer renderer)
     {
-        TransparentParm param = null;
+        TransparentParam param = null;
         transparentDic.TryGetValue(renderer, out param);
-        if(param == null)
+        if (param == null)
         {
-            param = new TransparentParm();
+            param = new TransparentParam();
             transparentDic.Add(renderer, param);
-            param.shaderMats = renderer.sharedMaterials;
+            //此处顺序不能反，调用material会产生材质实例。
+            param.sharedMats = renderer.sharedMaterials;
             param.materials = renderer.materials;
             foreach (var v in param.materials)
             {
---
+++ Assets/shader/肥皂泡/Lather.shader
@@ -0,0 +1,72 @@
+﻿Shader "Unlit/Lather"
+{
+    Properties
+    {
+        _Color("Main Color",Color) = (1,1,1,1)
+        _MetalRef("MetalRef",Range(0,1)) = 0
+
+        _MainTex("Main Tex",2D) = "white"{}
+        _BumpMap("Bumpmap(RGB)",2D) = "bump" {}
+        _BumpValue("BumpValue",Range(0,3)) = 1
+
+        _LightModle("LightModleDiffuse(RGB)",2D) = "white"{}
+        _LightModleValue("LightModleValue",Range(0,3)) = 1
+        _LightModleSpec("LightModleSpec(RGB)",2D) = "black" {}
+        _SpecValue("SpecValue",Range(0,4)) = 0
+
+        _Bubble("Bubble",2D) = "white"{}
+        _BubbleNoise("BubbleNoise",2D) = "white"{}
+        _BubbleValue("Bubble Value",Range(0,2)) = 1
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Transparent" "Queue" = "Transparent" }
+
+        Pass
+        {
+            Name "BASE"
+            Tags{"LightMode" = "Always"}
+            Blend SrcAlpha OneMinusSrcAlpha
+
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+            // make fog work
+            #pragma fragmentoption ARB_fog_exp2
+            #pragma fragmentoption ARB_precision_hint_fastest
+            #include "UnityCG.cginc"
+
+            struct v2f
+            {
+                float4 pos : SV_POSITION;
+                float4 uv : TEXCOORD0;
+                float3 TtoV0 : TEXCOORD1;
+                float3 TtoV1 : TEXCOORD2;
+                float3 visual : TEXCOORD3;
+                float4 normal : NORMAL;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+            v2f vert (appdata_tan v)
+            {
+                v2f o;
+                o.pos = UnityObjectToClipPos(v.vertex);
+                o.uv.xy = TRANSFORM_TEX(v.texcoord, _MainTex);
+                UNITY_TRANSFER_FOG(o,o.vertex);
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                // sample the texture
+                fixed4 col = tex2D(_MainTex, i.uv);
+                // apply fog
+                UNITY_APPLY_FOG(i.fogCoord, col);
+                return col;
+            }
+            ENDCG
+        }
+    }
+}
